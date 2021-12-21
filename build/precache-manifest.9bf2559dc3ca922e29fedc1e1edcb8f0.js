e84d75d6eb13fe1fd1c383866a4d28c6b60337dd
Paris Plain <paris@Pariss-MBP.localdomain>
2021-12-21 10:21:50 -0500

    Build added

---
+++ build/precache-manifest.9bf2559dc3ca922e29fedc1e1edcb8f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce12b5a7040e30a4a88285c1e843e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2759ba40fb9062f9aff0",
+    "url": "/static/css/2.ef2319b8.chunk.css"
+  },
+  {
+    "revision": "c7ddbb961001d9bdea36",
+    "url": "/static/css/main.2f864281.chunk.css"
+  },
+  {
+    "revision": "2759ba40fb9062f9aff0",
+    "url": "/static/js/2.2d55a600.chunk.js"
+  },
+  {
+    "revision": "6900c332b0ca3cc34c98d0aab7a926ff",
+    "url": "/static/js/2.2d55a600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ddbb961001d9bdea36",
+    "url": "/static/js/main.88022076.chunk.js"
+  },
+  {
+    "revision": "6001e515fb3b18de80d2",
+    "url": "/static/js/runtime-main.38f06a83.js"
+  },
+  {
+    "revision": "ec1ec64c21525d51ce3a4771bfd1804c",
+    "url": "/static/media/haapi.ec1ec64c.svg"
+  }
+]);
